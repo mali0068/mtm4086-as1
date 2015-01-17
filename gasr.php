b3b96cc9d77372b339b83ba2b4264f315efb23f0
Christian Griffin <grif0163@algonquinlive.com>
2015-01-17 17:17:09 -0500

    Added initial php code

--- gasr.php
+++ gasr.php
@@ -1,3 +1,33 @@
+<?php
+
+if(isset($_POST['btnSent'])){
+    //set the headers inside of a variable (so we can use them in the mail function later)
+    $headers = "From: Kristelle <macsween@rocketmail.com> \n";
+    // the \n is a newline character - and needs to be used to add things to the headers
+    $headers .= "Reply-To: Nick <nick@dede.com> \n";
+    // . is the PHP concatenation character - so .= adds to whatever is currently in the variable
+    //other headers: CC:, BCC:
+    $headers .= "Content-Type: text/plain; charset=\"utf-8\" \n";
+    //this sets the email type as just plain text
+
+    //get subject and message from user-input
+    $subject = trim($_POST['sub']);
+    $msg = trim($_POST['comm']);
+    //a plain text email just needs text - nothing special
+
+    //mail function: who the email is going to, the subject, the message, and any headers (if present)
+    $ret = mail('bob@bob.com', $subject, $msg, $headers);
+    if($ret){
+        echo "The email was successfully sent!";
+    }else{
+        echo "This mail server doesn't exist.";
+    }
+}
+
+?>
+
+
+
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -28,7 +58,7 @@
                 <h3>Game Broken?</h3>
                 <h4>...that's too bad.</h4>
             </div>
-            <form name="yourChar" id="yourChar" action="char.php" method="post">
+            <form name="yourChar" id="yourChar" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
             
             <div id="formleft">
                 <div class="formbox">
