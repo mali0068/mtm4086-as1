2a887a81b2dc17bacccb4d59e50c4bf3eb57b39b
muir0042 <muir0042@algonquinlive.com>
2015-01-20 16:47:34 -0500

    mess

---
+++ gasr.php
@@ -0,0 +1,125 @@
+<?php
+
+if(isset($_POST['btnSubmit'])){
+	
+	$email = $_POST['email'];
+ 	$headers  = "From: $email \r\n";
+    // the \n is a newline character - and needs to be used to add things to the headers
+    // . is the PHP concatenation character - so .= adds to whatever is currently in the variable
+    //other headers: CC:, BCC:
+    $headers .= "Mime-Version: 1.0 \n";
+    $headers .= "Content-Type: text/html; charset=\"utf-8\" \n";
+	
+	$mes ='<html>
+                <head>
+                </head>
+                <body>
+                    <h2>User Troubles</h2>
+                    <p>Problem: ' . $_POST['problem'] . '</p>
+					<p>Character: ' . $_POST['charType'] . '</p>
+					<p>Message: ' . trim($_POST['message']) . '</p>
+                </body>
+            </html>';
+
+    $ret = mail('muir0042@algonquinlive.com', 'User Troubles', $mes, $headers);
+
+}
+
+?>
+
+
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>GASR</title>
+<link href="styles/styles.css" rel="stylesheet" />
+</head>
+
+<body>
+
+    <div id="wrapper">
+        <div id="header">
+            <div class="awe" id="awesome1">
+                <p>Lightning!</p>
+                <p>Monsters!</p>
+                <p>Rendered Clouds!</p>
+            </div>
+            <div class="awe" id="awesome2">
+                <p>Massive Headers!</p>
+                <p>Random Chaos!</p>
+                <p>Unreadable Text!</p>
+            </div>
+        </div>
+        
+        <div id="formidable">
+        	<div id="subhead">
+                <p>Send us a message and maybe, if we feel like it, we'll look into it.</p>
+                <h3>Game Broken?</h3>
+                <h4>...that's too bad.</h4>
+            </div>
+            <form name="yourChar" id="yourChar" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+            
+            <div id="formleft">
+                <div class="formbox">
+                    <label for"email">Your E-mail:</label>
+                    <input type="text" id="email" class="charField" name="email" />
+                </div>
+                
+                <div class="formbox">
+                    <label for"charType">Your Character:</label>
+                    <select id="charType" name="charType" class="charField">
+                    	<option value="X">Select your character...</option>
+                        <option value="Compost Heap">Compost Heap</option>
+                        <option value="Felix">Felix</option>
+                        <option value="Robo-Pig">Robo-Pig</option>
+                        <option value="Slime Man">Slime Man</option>
+                        <option value="Sock Monster">Sock Monster</option>
+                    </select>            
+                </div>
+                
+                <div class="formbox">
+                    <label for"charJob">Message:</label>
+                    <textarea name="message" id="message" class="charField"></textarea>
+                </div>
+            </div>
+                
+            <div id="formright">                
+                <div class="formbox">
+                	<p>Type of problem:</p>
+                    <input type="radio" class="charField checkbox" id="rad1" name="problem" value="I played your game too much and my mouse/keyboard broke." />
+                    <label for="rad1">I played your game too much and my mouse/keyboard broke.</label>
+                    <input type="radio" class="charField checkbox" id="rad2" name="problem" value ="I have no significant other.  I need your sexy armours!"/>
+                	<label for="rad2">I have no significant other.  I need your sexy armours!</label>
+                    <input type="radio" class="charField checkbox" id="rad3" name="problem" value="My account was stolen by hackers."/>
+                    <label for="rad3">My account was stolen by hackers.</label>
+                    <input type="radio" class="charField checkbox" id="rad4" name="problem" value="I don't have a problem, I'm just lonely and want to talk to someone."/>
+                    <label for="rad4">I don't have a problem, I'm just lonely and want to talk to someone.</label>
+                </div>
+
+            </div>       
+              
+            <div class="formbox">
+                <input type="submit" id="btnSubmit" name="btnSubmit" class="button" value="Send!" />
+                <?php
+if(isset($_POST['btnSubmit'])){				
+    if($ret){
+        echo '<p>The email was successfully sent! May your ' . $_POST['charType'] . ' destroy all its enemies.</p>';
+    }else{
+        echo "This mail server doesn't exist.";
+    }	
+}
+				?>                  
+                
+            </div>
+            </form>
+        </div>
+        
+        <div id="footer">
+            Scary Stuff &copy Scary People
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ scripts/gasr_complete.js
@@ -0,0 +1,216 @@
+// JavaScript Document
+
+window.onload = superawesome;
+
+//this is in several parts...the idea here is to validate everything both onblur/onchange/onclick, and then also make it work when the user clicks the submit button.  this should provide a pretty comprehensive example for them to follow for their final!
+
+var theForm;
+
+function superawesome(){
+	//onclick for the button!  (not an input submit, but just the button element)
+	document.querySelector("#btnSubmit").onclick = processForm;
+	
+	//onchange for select menus
+	document.querySelector("#charType").onchange = getImage;
+	document.querySelector("#charJob").onchange = checkJob;
+	
+	//onblurs for the two text fields
+	document.querySelector("#email").onblur = checkEmail;
+	document.querySelector("#charName").onblur = checkName;
+	
+	//onclick for the checkbox
+	document.querySelector("#spam").onclick = checkChecker;
+	
+	//get the whole form and keep it in a global variable.  with the whole form we can call fields by name, like methods (if anyone cares, this is a javascript object)
+	theForm = document.querySelector("#yourChar");
+}
+
+//function that process the information in the form on submit
+function processForm(){	
+	//set an error message variable (start as an empty string)
+	var msg = "";
+	
+	//validate the name! (call a function to do this)
+	var name = checkName();
+	//check for a return...if it is returned, set error variable to false
+	if(name == true){
+		msg	= false;
+	}	
+	
+	//validate email!
+	var email = checkEmail();
+	//check for a return...
+	if(email == true){
+		msg = false;	
+	}
+	
+	//validate select menus
+	//this is easier if we use separate functions, because here we only have to check for false (if they have entered something correct, no reason to change it).  modifying the original functions to work both on submit and on change would be harder, though certainly possible.  something for them to consider in the future.
+	var charType = theForm.charType;
+	
+	if(charType.options[charType.selectedIndex].value == "X"){
+		charType.parentNode.childNodes[5].innerHTML = '<span class="error">Please choose a character.</span>';
+		msg = false;
+	}
+	
+	var job = theForm.charJob;
+	
+	if(job.options[job.selectedIndex].value == "X"){
+		job.parentNode.childNodes[5].innerHTML = '<span class="error">Please choose a job.</span>';
+		msg = false;
+	}
+	
+	
+	/*validate checkbox*/
+	var spam = checkChecker();
+	if(spam == true){
+		msg = false;	
+	}
+	
+	//finally, check for errors
+	if(msg == false){
+		//return false, normally, but we are always returning false	for testing purposes
+	}else{
+		//submit form
+	}
+	//alert(msg);
+	//prevent form from submitting
+	return false;
+}
+
+/**************************************************************************************************/
+
+/*******   check on blurs    *******/
+
+//check the name
+function checkName(){
+	//rules:  	must be longer than 1 characters, no more than 25
+	//			can not contain numbers
+	var name = theForm.charName;
+	//use a local message variable
+	var msg = "";
+	
+	//use the trim function (from the trim document) to get rid of leading and
+	//trailing spaces
+	nameVal = trim(name.value);
+	
+	//|| (double pipeline) is 'or'
+	if(nameVal.length < 2 || nameVal.length > 25){
+		msg += "Your name must be longer than a single charcter but no more than 25. ";
+	}
+	
+	//check for numbers
+	//isNaN = 'is not a number'
+	//use a for loop to loop through the characters and see if any of them are numbers (by checking if they are not numbers)
+	for(var n=0;n<nameVal.length;n++){
+		//get each character
+		var thisChar = nameVal.substring(n,n+1);
+		if(isNaN(thisChar) == false){
+			msg += "No numbers!";
+			break;
+		}
+	}
+	
+	//check for error message
+	if(msg != ""){
+		name.parentNode.childNodes[5].innerHTML = '<span class="error">' + msg + '</span>';
+		return true;
+	}else{
+		name.parentNode.childNodes[5].innerHTML = '<span class="correct">A good, strong name.</span>';
+	}
+}
+
+function checkEmail(){
+	//rules:	must have @ symbol, must have . after @
+	//			only one @ symbol, at least one .
+	//			the last . must be at least two characters from the end
+	//			the @ cannot be the first character
+	var emailElem = theForm.email;
+	var email = theForm.email.value;
+	
+	//get the positions of various things in the email
+	var firstAt = email.indexOf("@");
+	var lastAt = email.lastIndexOf("@");
+	var firstDot = email.indexOf(".");
+	var lastDot = email.lastIndexOf(".");
+	var wrongEmail = false;
+	
+	//check for position of @ symbol
+	//can't be first - so less than 1 covers position or position -1
+	//can't be more than one, so first position must also be last position
+	if(firstAt < 1 || firstAt != lastAt){
+		//firstAt == 0: @ is first character
+		//firstAt == -1: there is no @
+		//firstAt != lastAt: there is more than one @
+		wrongEmail = true;
+	}
+	
+	//check for position of dot
+	//must be at least one, so position can't be -1
+	//one must come after last @, so last position must be greater than last at
+	//must be at least 2 characters after the last dot, so last dot must be at least 2 places from the length
+	if(firstDot == -1 || lastDot < lastAt || (lastDot >= email.length-2)){
+		//firstDot == -1: no .
+		//lastDot < lastAt: no . after @
+		//lastDot+1 >= email.length-2: the dot is one of the last two characters
+		wrongEmail = true;
+	}
+	
+	//finally, enter the necessary message
+	if(wrongEmail == true){
+		emailElem.parentNode.childNodes[5].innerHTML = '<span class="error">Please enter a valid email.</span>';
+		return true;
+	}else{
+		emailElem.parentNode.childNodes[5].innerHTML = '<span class="correct">What a lovely email.</span>';
+	}
+}
+
+
+/*    onclick for checkbox function    */
+function checkChecker(){
+	var spam = theForm.spam;
+	if(spam.checked == false){
+		spam.parentNode.childNodes[5].innerHTML = '<span class="error">You must accept spam to play this game!</span>';	
+		return true;
+	}else{
+		spam.parentNode.childNodes[5].innerHTML = '<span class="correct">Selling your soul to us keeps this game free.</span>';	
+	}
+}
+
+/*   onchange functions for select menus   */
+function getImage(){
+	//check to see if they chose a character
+	var sel = this.options[this.selectedIndex];
+	//get div
+	var div = this.parentNode.childNodes[5];
+	
+	if(sel.value == "X"){
+		//alert error
+		div.innerHTML = '<span class="error">Please choose a character</span>';
+	}else{
+		//get image
+		var img = (sel.text);
+		img = img.toLowerCase();
+		img = img + ".gif";
+		
+		//make innerHTML
+		div.innerHTML = '<img src="images/' + img + '" class="imgFloat" />' + '<span class="correct">Your character choice says something about you.</span>';
+	}
+}
+
+function checkJob(){
+	//check to see if they chose a character
+	var sel = this.options[this.selectedIndex];
+	//get div
+	var div = this.parentNode.childNodes[5];
+	
+	if(sel.value == "X"){
+		//alert error
+		div.innerHTML = '<span class="error">Please choose a job</span>';
+	}else{
+		div.innerHTML = '<span class="correct">A good choice of job.</span>';
+	}
+}
+
+
+
