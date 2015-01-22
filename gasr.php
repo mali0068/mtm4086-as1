1db550c6828add1df377747c97ab68470282964a
Becky Putnam <putn0002@algonquinlive.com>
2015-01-22 16:43:19 -0500

    a magical commit

--- gasr.php
+++ gasr.php
@@ -1,3 +1,41 @@
+<?php
+
+//Plain text email!
+
+if(isset($_POST['btnSubmit'])){
+
+    //set variables to pull the users information
+    $email = trim($_POST['email']);
+    $char = trim($_POST['charType']);
+    $msg = trim($_POST['message']);
+    $prob = trim($_POST['problem']);
+
+    $userinfo = $email . "is playing " . $char . ".\nThe problem is " . $prob . "\n" . $email . "also says: " . $msg;
+    // set the headers inside of a variable (so we can use them in the mail function later)
+
+    $headers = "From: " . $email . "\n";   
+
+    $headers .= "Reply-To: broken@gasr.com \n"; 
+
+    //This will set the email type to plain text
+    $headers .= "Content-Type: text/plain; charset=\"utf-8\" \n";
+
+    // get subject and message from the problem
+    $subject = trim($_POST['problem']);
+
+    // mail function: to, subject, message, and aheaders
+    $ret = mail('broken@gasr.com',$subject, $userinfo, $headers);
+    if($ret){
+        echo "Your email has been sent successfully!";
+    }else{
+        echo "You email was not delivered :(";
+    }
+}
+
+
+?>
+
+
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -28,7 +66,7 @@
                 <h3>Game Broken?</h3>
                 <h4>...that's too bad.</h4>
             </div>
-            <form name="yourChar" id="yourChar" action="char.php" method="get">
+            <form name="yourChar" id="yourChar" action= "gasr.php" method="post">
             
             <div id="formleft">
                 <div class="formbox">
@@ -40,11 +78,11 @@
                     <label for"charType">Your Character:</label>
                     <select id="charType" name="charType" class="charField">
                     	<option value="X">Select your character...</option>
-                        <option value="CH">Compost Heap</option>
-                        <option value="F">Felix</option>
-                        <option value="RP">Robo-Pig</option>
-                        <option value="SL">Slime Man</option>
-                        <option value="SM">Sock Monster</option>
+                        <option value="Compost Heap">Compost Heap</option>
+                        <option value="Felix">Felix</option>
+                        <option value="Robo-Pig">Robo-Pig</option>
+                        <option value="Slime Man">Slime Man</option>
+                        <option value="Sock Monster">Sock Monster</option>
                     </select>            
                 </div>
                 
@@ -57,13 +95,13 @@
             <div id="formright">                
                 <div class="formbox">
                 	<p>Type of problem:</p>
-                    <input type="radio" class="charField checkbox" id="rad1" name="problem" />
+                    <input type="radio" class="charField checkbox" id="rad1" name="problem" value="I played your game too much and my mouse/keyboard broke."/>
                     <label for="rad1">I played your game too much and my mouse/keyboard broke.</label>
-                    <input type="radio" class="charField checkbox" id="rad2" name="problem" />
+                    <input type="radio" class="charField checkbox" id="rad2" name="problem" value="I have no significant other.  I need your sexy armours!" />
                 	<label for="rad2">I have no significant other.  I need your sexy armours!</label>
-                    <input type="radio" class="charField checkbox" id="rad3" name="problem" />
+                    <input type="radio" class="charField checkbox" id="rad3" name="problem" value="My account was stolen by hackers." />
                     <label for="rad3">My account was stolen by hackers.</label>
-                    <input type="radio" class="charField checkbox" id="rad4" name="problem" />
+                    <input type="radio" class="charField checkbox" id="rad4" name="problem" value="I don't have a problem, I'm just lonely and want to talk to someone." />
                     <label for="rad4">I don't have a problem, I'm just lonely and want to talk to someone.</label>
                 </div>
             </div>       
