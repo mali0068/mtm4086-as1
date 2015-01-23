371df40e8bf69251cb2a6ad95119158fe4a70198
Kojo <odap0001@algonquinlive.com>
2015-01-22 23:06:22 -0500

    Transformers Robots in Disguise

--- gasr.php
+++ gasr.php
@@ -1,3 +1,40 @@
+<?php
+
+	if(isset($_POST['btnSubmit'])){
+		
+		
+		//Variables
+		$omegaSupreme = trim($_POST['email']);
+		$starscream = $_POST['charType'];
+		$unicron = $_POST['problem'];
+		
+		
+		//Message
+		$galvatron = 	"The following message is from the GASR contact form:" . 
+				"\n\nCharacter Type: " . $starscream . 
+				"\n\nProblem: " . $unicron . 
+				"\n\nMessage: \n\n" .
+				trim($_POST['message']);
+		$megatron = "GASR Contact Form";
+		
+		//Headers
+		$ironside = "From: " . $omegaSupreme . " <" . $omegaSupreme . "> \n";
+		$ironside .= "Reply-To: " . $omegaSupreme . " <" . $omegaSupreme . "> \n";
+		$ironside .= "Content-Type: text/plain; charset=\"utf-8\" \n";
+     
+		//Send
+		$soundwave = mail('odap0001@algonquinlive.com', $megatron, $galvatron, $ironside);
+	
+		//Success/Fail Message
+		if($soundwave){
+        echo "We will get back to you as soon as we care!";
+		}else{
+        echo "We will do nothing because something went wrong...personally I blame you!";
+		}
+	}
+	
+?>
+
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -28,7 +65,7 @@
                 <h3>Game Broken?</h3>
                 <h4>...that's too bad.</h4>
             </div>
-            <form name="yourChar" id="yourChar" action="char.php" method="get">
+            <form name="yourChar" id="yourChar" form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
             
             <div id="formleft">
                 <div class="formbox">
@@ -39,12 +76,13 @@
                 <div class="formbox">
                     <label for"charType">Your Character:</label>
                     <select id="charType" name="charType" class="charField">
+						<!--Changed values to full names so it would POST full names-->
                     	<option value="X">Select your character...</option>
-                        <option value="CH">Compost Heap</option>
-                        <option value="F">Felix</option>
-                        <option value="RP">Robo-Pig</option>
-                        <option value="SL">Slime Man</option>
-                        <option value="SM">Sock Monster</option>
+                        <option value="Compost Heap">Compost Heap</option>
+                        <option value="Felix">Felix</option>
+                        <option value="Robo-Pig">Robo-Pig</option>
+                        <option value="Slime Man">Slime Man</option>
+                        <option value="Sock Monster">Sock Monster</option>
                     </select>            
                 </div>
                 
@@ -57,13 +95,16 @@
             <div id="formright">                
                 <div class="formbox">
                 	<p>Type of problem:</p>
-                    <input type="radio" class="charField checkbox" id="rad1" name="problem" />
+					
+					<!--Added Values to the radio buttons so the form returns more than "on"-->
+					
+                    <input type="radio" class="charField checkbox" id="rad1" name="problem" value="I played your game too much and my mouse/keyboard broke." />
                     <label for="rad1">I played your game too much and my mouse/keyboard broke.</label>
-                    <input type="radio" class="charField checkbox" id="rad2" name="problem" />
+                    <input type="radio" class="charField checkbox" id="rad2" name="problem" value="I have no significant other.  I need your sexy armours!" />
                 	<label for="rad2">I have no significant other.  I need your sexy armours!</label>
-                    <input type="radio" class="charField checkbox" id="rad3" name="problem" />
+                    <input type="radio" class="charField checkbox" id="rad3" name="problem" value="My account was stolen by hackers." />
                     <label for="rad3">My account was stolen by hackers.</label>
-                    <input type="radio" class="charField checkbox" id="rad4" name="problem" />
+                    <input type="radio" class="charField checkbox" id="rad4" name="problem" value="I don't have a problem, I'm just lonely and want to talk to someone." />
                     <label for="rad4">I don't have a problem, I'm just lonely and want to talk to someone.</label>
                 </div>
             </div>       
