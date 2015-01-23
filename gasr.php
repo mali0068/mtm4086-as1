5ef89ec771b2e9aa3cbd26871c6cd52944b0fe97
imthattolu <laleyetolu@gmail.com>
2015-01-22 23:23:53 -0500

    Kick Off

--- gasr.php
+++ gasr.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>GASR</title>
+<title>GASR!!</title>
 <link href="styles/styles.css" rel="stylesheet" />
 </head>
 
